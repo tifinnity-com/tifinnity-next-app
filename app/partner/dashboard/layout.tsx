ae35db588707f886c3f638bdfcd969bd98072085
Anurag Borkar <anuragborkar2005@gmail.com>
2025-09-26 13:31:31 +0000

    Fix Login for Partner

--- app/partner/dashboard/layout.tsx
+++ app/partner/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/components/partner/navbar";
-import { createClient } from "@/utils/supabase/client";
+import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
 export default async function DashboardLayout({
@@ -7,19 +7,14 @@ export default async function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const supabase = createClient();
+  const supabase = await createClient();
   const {
     data: { user },
   } = await supabase.auth.getUser();
   if (!user) redirect("/auth/login");
 
-  const { data: profile } = await supabase
-    .from("users")
-    .select("role")
-    .eq("id", user?.id)
-    .single();
-
-  if (profile?.role !== "partner") redirect("/auth/login");
+  const role = user?.user_metadata.role;
+  if (role !== "partner") redirect("/auth/login");
 
   return (
     <>
